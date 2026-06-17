874ea84a1e548e32ca1ff7531f5b2538b52c39b4
smitghotki <smitghotki@gmail.com>
2026-06-17 15:18:05 +0500

    task 3 done

---
+++ task3.js
@@ -0,0 +1,29 @@
+let days=prompt("Enter any number from 1 to 7")
+
+days=Number(days)
+
+switch(days){
+    case 1:
+        console.log("It is monday");
+        break;
+    case 2:
+        console.log("It is Tuesday");
+        break;
+    case 3:
+        console.log("It is Wednesday");
+        break;
+    case 4:
+        console.log("It is Thursday");
+        break;
+    case 5:
+        console.log("It is Friday");
+        break;
+    case 6:
+        console.log("It is Saturday");
+        break;
+    case 7:
+        console.log("It is Sunday");
+        break;
+    default:
+        console.log("Invalid input");
+}
