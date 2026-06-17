c0d2040a2137bf0111a33b0c000096dd06fc849e
smitghotki <smitghotki@gmail.com>
2026-06-17 14:57:50 +0500

    first task done

---
+++ task1.js
@@ -0,0 +1,15 @@
+let ch=prompt("Type any character from a to z")
+
+if(ch=='a')
+    console.log("It is Vowel");
+else if(ch=='e')
+    console.log("It is Vowel");
+else if(ch=='i')
+    console.log("It is Vowel");
+else if(ch=='o')
+    console.log("It is Vowel");
+else if(ch=='u')
+    console.log("It is Vowel");
+else {
+    console.log("It is not vowel");
+}
